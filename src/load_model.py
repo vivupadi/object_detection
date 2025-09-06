7c89a3d8f85e6de4af395bea29b6e86c46db7c09
vivupadi <vivupadi@gmail.com>
2025-09-06 23:34:00 +0200

    recommitting after removing old files

---
+++ src/load_model.py
@@ -0,0 +1,119 @@
+import torch
+#import hub
+import tensorflow_hub as hub
+
+import cv2
+import time
+import tensorflow as tf
+
+from preprocess import *
+
+#Input For video based object detection
+"""comment out while working with object detection on image"""
+filename = "C:\\Users\\Vivupadi\\Downloads\\PXL_20250802_072807827.mp4"
+##########
+
+#Input For image based object detection
+"""comment out while working with object detection on video"""
+downloaded_image_path = "C:\\Users\\Vivupadi\\Downloads\\PXL_20250603_034803966.jpg"
+downloaded_image_path = download_and_resize_image(downloaded_image_path, 1380, 950, True)
+##########
+
+
+
+#Load model
+module_handle = "https://tfhub.dev/tensorflow/ssd_mobilenet_v2/2"
+detector = hub.load(module_handle).signatures['serving_default']
+#########
+
+
+
+def load_img(path):
+    img = tf.io.read_file(path)
+    img = tf.image.decode_jpeg(img, channels=3)
+    return img
+
+def load_labels(path = 'coco_label.txt'):
+    with open(path, 'r') as f:
+        label = f.read().splitlines()
+    return label
+
+def run_detector(detector, path):
+    img = load_img(path)
+
+    converted_img  = tf.image.convert_image_dtype(img, tf.float32)[tf.newaxis, ...]
+    start_time = time.time()
+    
+    # Load image using OpenCV
+    img = cv2.imread(path)
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+    # Expand dims to make it (1, H, W, 3)
+    input_tensor = tf.expand_dims(img_rgb, axis=0)
+
+    # Make sure it's uint8
+    converted_img = tf.cast(input_tensor, tf.uint8)
+
+    result = detector(converted_img)
+    #breakpoint()
+    end_time = time.time()
+
+    result = {key:value.numpy() for key,value in result.items()}
+
+    print("Found %d objects." % len(result["detection_scores"]))
+    print("Inference time: ", end_time-start_time)
+
+    boxes = np.array(result["detection_boxes"][0])  # shape: [num_detections, 4]
+    classes = np.array(result["detection_classes"][0]).astype(int)  # shape: [num_detections]
+    scores = np.array(result["detection_scores"][0])  # shape: [num_detections]
+
+    #breakpoint()
+    image_with_boxes = draw_boxes(img, boxes, classes, scores, label_map = coco_labels)
+
+    display_image(image_with_boxes)
+
+#Function to handle video input
+def preprocess_video_and_run_detector(filename):
+    video = cv2.VideoCapture(filename)
+
+    while video.isOpened():
+        ret, fFrame = video.read()
+        if not ret:
+            break
+        
+        img_rgb = cv2.cvtColor(fFrame, cv2.COLOR_BGR2RGB)
+
+        # Expand dims to make it (1, H, W, 3)
+        input_tensor = tf.expand_dims(img_rgb, axis=0)
+
+        # Make sure it's uint8
+        converted_img = tf.cast(input_tensor, tf.uint8)
+
+        result = detector(converted_img)
+
+        result = {key:value.numpy() for key,value in result.items()}
+
+        boxes = np.array(result["detection_boxes"][0])  # shape: [num_detections, 4]
+        classes = np.array(result["detection_classes"][0]).astype(int)  # shape: [num_detections]
+        scores = np.array(result["detection_scores"][0])  # shape: [num_detections]
+
+        #breakpoint()
+        image_with_boxes = draw_boxes(fFrame, boxes, classes, scores, label_map = coco_labels)
+
+        #display_image(image_with_boxes)
+        display_video_object(image_with_boxes)
+
+        # Press Q to quit
+        if cv2.waitKey(1) & 0xFF == ord("q"):
+            break
+    video.release()
+    cv2.destroyAllWindows()
+
+coco_labels= load_labels('coco_label.txt')
+
+
+#To detect Images(comment out while working with object detection on video)
+run_detector(detector, downloaded_image_path)
+
+#To detect objects in videos (comment out while working with object detection on image)
+#preprocess_video_and_run_detector(filename)
---
+++ src/preprocess.py
@@ -0,0 +1,101 @@
+#Preprocessing for object detection
+import tempfile
+import matplotlib.pyplot as plt
+import cv2
+from PIL import Image, ImageOps, ImageDraw, ImageColor, ImageFont
+import numpy as np
+from urllib.request import urlopen
+import requests
+from io import BytesIO
+
+def display_image(image):
+  fig = plt.figure(figsize=(15, 10))
+  plt.grid(False)
+  plt.imshow(image)
+  plt.show()
+
+
+def display_video_object(image, window_name="Detection"):
+    cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
+    cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+    cv2.imshow(window_name, image )
+    return True
+
+
+def download_and_resize_image(img, new_width=256, new_height=256, display=False):
+  _, filename = tempfile.mkstemp(suffix=".jpg")
+  pil_image = Image.open(img)
+  pil_image = ImageOps.fit(pil_image, (new_width, new_height), Image.LANCZOS)
+  pil_image_rgb = pil_image.convert("RGB")
+  pil_image_rgb.save(filename, format="JPEG", quality=90)
+  print("Image downloaded to %s." % filename)
+  #if display:
+    #display_image(pil_image)
+  return filename
+
+
+def draw_bounding_box_on_image(image, ymin, xmin, ymax, xmax, color, font, thickness=4, display_str_list=()):
+  """Adds a bounding box to an image."""
+  draw = ImageDraw.Draw(image)
+  im_width, im_height = image.size
+  (left, right, top, bottom) = (xmin * im_width, xmax * im_width,
+                                ymin * im_height, ymax * im_height)
+  draw.line([(left, top), (left, bottom), (right, bottom), (right, top),
+             (left, top)],
+            width=thickness,
+            fill=color)
+
+  # If the total height of the display strings added to the top of the bounding
+  # box exceeds the top of the image, stack the strings below the bounding box
+  # instead of above.
+  display_str_heights = [font.getbbox(ds)[3] for ds in display_str_list]
+  # Each display_str has a top and bottom margin of 0.05x.
+  total_display_str_height = (1 + 2 * 0.05) * sum(display_str_heights)
+
+  if top > total_display_str_height:
+    text_bottom = top
+  else:
+    text_bottom = top + total_display_str_height
+  # Reverse list and print from bottom to top.
+  for display_str in display_str_list[::-1]:
+    bbox = font.getbbox(display_str)
+    text_width, text_height = bbox[2], bbox[3]
+    margin = np.ceil(0.05 * text_height)
+    draw.rectangle([(left, text_bottom - text_height - 2 * margin),
+                    (left + text_width, text_bottom)],
+                   fill=color)
+    draw.text((left + margin, text_bottom - text_height - margin),
+              display_str,
+              fill="black",
+              font=font)
+    text_bottom -= text_height - 2 * margin
+
+
+def draw_boxes(image, boxes, class_names, scores, max_boxes=10, min_score=0.55, label_map=None):
+    """Overlay labeled boxes on an image with formatted scores and label names."""
+    colors = list(ImageColor.colormap.values())
+        
+    try:
+        font = ImageFont.truetype("arial.ttf", 15)
+    except IOError:
+        print("Font not found, using default font.")
+        font = ImageFont.load_default(size=15)
+    
+
+    #breakpoint()
+    for i in range(min(boxes.shape[0], max_boxes)):
+        if scores[i] >= min_score:
+            ymin, xmin, ymax, xmax = tuple(boxes[i])
+
+            class_id = int(class_names[i])
+            if label_map and 1<= class_id <= len(label_map):
+               label = label_map[class_id - 1]
+            else:
+               label = str(class_id)
+
+            display_str = "{}: {}%".format(label,int(100 * scores[i]))
+            color = colors[hash(class_names[i]) % len(colors)]
+            image_pil = Image.fromarray(np.uint8(image)).convert("RGB")
+            draw_bounding_box_on_image(image_pil, ymin, xmin, ymax, xmax, color, font, display_str_list=[display_str])
+            np.copyto(image, np.array(image_pil))
+    return image
